35c929e8f42a431aad5a0192758315b1ac208260
nacrow <529944053@qq.com>
2021-09-26 17:25:26 -0400

    Update Main Program with test cases

--- src/main/java/edu/neu/coe/info6205/assignment2/MainProgram.java
+++ src/main/java/edu/neu/coe/info6205/assignment2/MainProgram.java
@@ -11,15 +11,13 @@ import java.util.function.Supplier;
 
 public class MainProgram {
     public static void main (String[] args){
-        Timer timer = new Timer();
-        int n = 50;
-        InsertionSort is = new InsertionSort();
-        Function sort = new Function() {
-            @Override
-            public Object apply(Object o) {
-                return is.sort((List)o);
-            }
-        };
+        // int n = 1250;
+        // int n = 2500;
+        // int n = 5000;
+        // int n = 10000;
+        // int n = 20000;
+         int n = 40000;
+        // int n = 80000;
         Supplier randomArray = new Supplier() {
             @Override
             public Object get() {
@@ -39,8 +37,9 @@ public class MainProgram {
                 for (int i = 0;i < n;i++){
                     if (i < n/2){
                         array.add(i);
+                    } else {
+                        array.add(rd.nextInt(n * 2) + n/2);
                     }
-                    array.add(rd.nextInt(n * 2) + n/2);
                 }
                 return array;
             }
@@ -59,12 +58,35 @@ public class MainProgram {
             @Override
             public Object get() {
                 List<Integer> array = new ArrayList<>();
-                for (int i = n - 1;i > 0;i--){
+                for (int i = n - 1;i >= 0;i--){
                     array.add(i);
                 }
                 return array;
             }
         };
-        System.out.print(timer.repeat(100,randomArray,sort));
+        System.out.print("Run insertion sort on a random Array with n = " + n +" :" + new Timer().repeat(100, randomArray, new Function() {
+            @Override
+            public Object apply(Object o) {
+                return new InsertionSort().sort((List)o);
+            }
+        })+"\n");
+        System.out.print("Run insertion sort on a ordered Array with n = " + n +" :" + new Timer().repeat(100, orderedArray, new Function<Object, Object>() {
+            @Override
+            public Object apply(Object o) {
+                return new InsertionSort().sort((List)o);
+            }
+        })+"\n");
+        System.out.print("Run insertion sort on a partly-ordered Array with n = " + n +" :" + new Timer().repeat(100,partlyOrdered,new Function<Object, Object>() {
+            @Override
+            public Object apply(Object o) {
+                return new InsertionSort().sort((List)o);
+            }
+        })+"\n");
+        System.out.print("Run insertion sort on a reversed Array with n = " + n +" :" + new Timer().repeat(100,reversedArray,new Function<Object, Object>() {
+            @Override
+            public Object apply(Object o) {
+                return new InsertionSort().sort((List)o);
+            }
+        })+"\n");
     }
 }
